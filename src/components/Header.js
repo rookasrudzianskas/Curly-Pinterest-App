303791b0ec0117755be858586bb5fe8858b8fda1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 05:23:56 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,28 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+            <Wrapper>
+                <LogoWrapper>
+
+                </LogoWrapper>
+                <HomePageButton>
+
+                </HomePageButton>
+
+                <FollowingButton>
+
+                </FollowingButton>
+
+                <SearchWrapper>
+                    <SearchBarWrapper>
+
+                    </SearchBarWrapper>
+                </SearchWrapper>
+
+                <IconsWrapper>
+
+                </IconsWrapper>
+            </Wrapper>
         </div>
     );
 };
