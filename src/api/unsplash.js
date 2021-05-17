1996264449b0f782939a6c07faad7688b363b2a2
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 06:25:57 +0300

    Roaks

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,2 +1,8 @@
 import axios from "axios";
 
+export default axios.create({
+    baseURL: "https://api.unsplash.com/",
+    headers: {
+        Authorization: "Client-ID GGbEa6T72TfhMjIb5gMNzfC_foUCnK1D0DjoJ7_oWdo"
+    }
+})
